8c4b9379e9b81cdf5cb91ca49e08a437325625c3
José Albornoz <josealbornozsalazar@gmail.com>
2026-07-18 20:17:44 -0400

    Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
 
 export default defineConfig({
   integrations: [vue()],
-  site: 'https://asprossemillas.com',
+  site: "https://joseTINegocios.github.io",
+  base: "/Aspros-Page/",
 });
